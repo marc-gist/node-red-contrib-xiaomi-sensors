a1fec210def88473370e4a39156e9bf10c1fb725
Marc <marc@refamco.ca>
2019-03-07 14:53:41 -0500

    status work

--- button_v2/button_v2.js
+++ button_v2/button_v2.js
@@ -4,49 +4,52 @@ module.exports = function(RED) {
     const moment = require('moment');
     const fmt = 'YYYY-MM-DD HH:mm';
 
-    RED.nodes.registerType('button_v1', function (config) {
+    RED.nodes.registerType('button_v2', function (config) {
         RED.nodes.createNode(this, config);
         this.lowBatteryLevel = config.lowBatteryLevel;
         var node = this;
-
         //not this is "global" and saved states
         node.on('input', function (msg) {
-            var ret = [null, null, null, null, null];
-            msg.payload = JSON.parse(msg.payload);
+            let ret = [null, null, null, null, null];
+            let etype = "";
             let lowBat = false;
-            let btype = "";
-
+            msg.payload = JSON.parse(msg.payload);
             // if(msg.payload.contact === true) {
             //     // door is closed, second position payload.
             //     ret[0] = msg;
             // } else {
             //     ret[1] = msg;
             // }
+            let pos = -1;
             if(msg.payload.hasOwnProperty('click')) {
-                btype = msg.payload.click;
-                var pos = -1;
                 if(msg.payload.click === 'single')
                     pos = 0;
                 else if(msg.payload.click === 'double')
                     pos = 1;
-                else if(msg.payload.click === 'triple')
-                    pos = 2;
-                else if(msg.payload.click === 'quadruple')
-                    pos = 3;
-                if(pos >= 0 && pos <=3)
-                    ret[pos] = msg;
+
+                etype = msg.payload.click;
             }
+            if(msg.payload.hasOwnProperty('action')) {
+                if(msg.payload.action === 'hold')
+                    pos = 2;
+                else if(msg.payload.action === 'release')
+                    pos = 3;
+
+                etype = msg.payload.action;
+            }
+            if(pos >= 0 && pos <=3)
+                ret[pos] = msg;
+
             if(msg.payload.battery < node.lowBatteryLevel) {
                 ret[4] = msg;
                 lowBat = true;
             }
 
             node.status({
-                text: btype + " @ " + moment().format(fmt),
+                text: etype + " @ " + moment().format(fmt),
                 fill: lowBat ? "red" : "green",
                 shape: "dot"
             });
-
             node.send(ret);
         });
     });
