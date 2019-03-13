a0c55a66cdfab13de045461f37b0d8a657e2d28e
Marc <marc@refamco.ca>
2019-03-13 16:49:34 -0400

    motion delay empty notification

--- motion_sensor/motion-sensor.js
+++ motion_sensor/motion-sensor.js
@@ -10,25 +10,60 @@ module.exports = function(RED) {
     RED.nodes.registerType('motion-sensor', function (config) {
         RED.nodes.createNode(this, config);
         this.lowBatteryLevel = config.lowBatteryLevel;
+        this.motionDelay = config.motionDelay * 1000; // seconds so *1000 for ms timer
+        this.nodeIcon = "";
+        this.nodeColor = "";
+        this.delayStatus = null;
+
         var node = this;
-        //not this is "global" and saved states
-        node.on('input', function (msg) {
-            var ret = [null, null, null];
-            msg.payload = JSON.parse(msg.payload);
+
+        node.delayMessage = function(msg) {
             node.status({
                 text: moment().format(fmt),
-                fill: msg.payload.occupancy ? "red" : "blue",
-                shape: msg.payload.occupancy ? "dot" : "ring"
+                fill: "blue",
+                shape: "dot"
             });
+            node.send([null, msg, null]);
+        };
+
+        //not this is "global" and saved states
+        node.on('input', function (msg) {
+            //["Occupied", "Empty", "Low Battery"]
+            let ret = [null, null, null];
+            msg.payload = JSON.parse(msg.payload);
+
             if(msg.payload.occupancy === true) {
                 ret[0] = msg;
+                if(node.motionDelay > 0)
+                    clearTimeout(node.delayStatus);
+                node.nodeColor = "red";
             } else {
-                ret[1] = msg;
+                if(node.motionDelay > 0 ) {
+                    node.delayStatus = setTimeout(node.delayMessage, node.motionDelay, RED.util.cloneMessage(msg));
+                    node.nodeColor = "yellow";
+                } else {
+                    ret[1] = msg;
+                    node.nodeColor = "blue";
+                }
             }
+
             if(msg.payload.battery < node.lowBatteryLevel) {
                 ret[2] = msg;
             }
+
+            node.status({
+                text: moment().format(fmt),
+                fill: node.nodeColor,
+                shape: "dot"
+            });
+
             node.send(ret);
+        });
+
+        node.on('close', function() {
+            if(node.motionDelay > 0)
+                clearTimeout(node.delayStatus);
+            done();
         });
     });
 };
