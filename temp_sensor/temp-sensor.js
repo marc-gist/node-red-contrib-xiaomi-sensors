f662d8bc8d38d32e61ec6e39232fdfa1369c6ec9
Marc <marc@refamco.ca>
2019-03-12 20:25:01 -0400

    temp output with defaults fix

--- temp_sensor/temp-sensor.js
+++ temp_sensor/temp-sensor.js
@@ -7,30 +7,55 @@ module.exports = function(RED) {
     RED.nodes.registerType('temp-sensor', function (config) {
         RED.nodes.createNode(this, config);
         this.lowBatteryLevel = config.lowBatteryLevel;
+        this.rangeAbove = config.rangeAbove;
+        this.rangeBelow = config.rangeBelow;
         let node = this;
         //not this is "global" and saved states
         node.on('input', function (msg) {
             let ret = [null, null, null, null];
             let alert = false;
 
+            let newMsg = {
+               payload: null,
+               topic: msg.topic,
+               data: msg.payload
+            };
+
             msg.payload = JSON.parse(msg.payload);
-            const t = msg.payload.temperature;
+
+            ret[0] = Object.assign({}, newMsg);
+            ret[0].payload = Number(msg.payload.temperature.toFixed(1));
+            ret[1] = Object.assign({}, newMsg);
+            ret[1].payload = Number(msg.payload.humidity.toFixed(1));
+
             if(node.rangeBelow && msg.payload.temperature < node.rangeBelow) {
                 alert = true;
                 ret[2] = {
                     payload: true,
-                    data: msg.payload
+                    data: msg.payload,
+                    alert: 'below'
                 }
             }
+            if(node.rangeAbove && msg.payload.temperature > node.rangeAbove) {
+                alert = true;
+                ret[2] = {
+                    payload: true,
+                    data: msg.payload,
+                    alert: 'above'
+                }
+            }
+
             node.status({
                 text: moment().format(fmt),
-                fill: msg.payload.contact ? "green" : "red",
-                shape: msg.payload.contact ? "dot" : "ring"
+                fill: alert ? "red" : "green",
+                shape: alert ? "dot" : "ring"
             });
 
             if(msg.payload.battery < node.lowBatteryLevel) {
                 ret[3] = msg;
             }
+
+            //send node output
             node.send(ret);
         });
     });
